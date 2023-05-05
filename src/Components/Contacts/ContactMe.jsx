03efaefbd2095bec1cb6319d554f0bab41ad6766
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-05-05 10:38:40 +0100

    new change

--- src/Components/Contacts/ContactMe.jsx
+++ src/Components/Contacts/ContactMe.jsx
@@ -4,12 +4,29 @@ import {HiLocationMarker} from 'react-icons/hi';
 import {BsFillTelephoneOutboundFill} from 'react-icons/bs';
 import {MdAttachEmail} from 'react-icons/md';
 import {IoLogoWhatsapp} from 'react-icons/io';
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
+import emailjs from '@emailjs/browser';
 
 
+ 
 export const ContactMe = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+     
+  
+      emailjs.sendForm('service_2ijos78', 'template_yocf07d', form.current, 'TN_LogzUsxIiZdUEb')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+  
+
     useEffect(function () {
         Aos.init({ duration: 1000 });
       }, []);
@@ -19,8 +36,8 @@ export const ContactMe = () => {
         <div className="min-h-[100vh] overflow-x-0 rounded after:bg-Contact p-[30px] bg-fixed after:min-h-[100%] relative after:absolute after:top-0 after:left-0 after:right-0 after:w-full bg-cover bg-no-repeat bg-[url('Components/Contacts/nzube.png')] ">
             <div data-aos='fade-up' data-aos-duration='1000' className="relative z-10">
             <div className="text-start pb-[20px] pt-[80px] ">
-                <h1 className="font-myfont text-slate-50 uppercase text-[40px] ">Contact Me</h1>
-                <p className="text-[20px] text-slate-300 font-fonty  ">Get in touch with me</p>
+                <h1 className="font-myfont text-slate-50 uppercase text-[30px] md:text-[40px] ">Contact Me</h1>
+                <p className="md:text-[20px]  text-[15px] text-slate-300 font-fonty  ">Get in touch with me</p>
             </div>
             <div className="flex flex-col  gap-[20px] items-start justify-center ">
             <div className="relative flex flex-col md:flex-row gap-5 z-10">
@@ -35,20 +52,21 @@ export const ContactMe = () => {
                     </a>
                     <a className="flex flex-row gap-1 items-center " href="tel:+2347084183411"><BsFillTelephoneOutboundFill className="text-slate-50 text-[25px] "/><span className="text-[15px] text-slate-100 font-fonty ">+234 708 4183 611</span> </a>
                                     </div>
-                <form action="" className="grid grid-cols-1 w-full md:grid-cols-2 gap-5">
+                <form action="" ref={form} onSubmit={sendEmail} className="grid grid-cols-1 w-full md:grid-cols-2 gap-5">
                     <div>
                         <label className="text-slate-100 text-[20px] font-fonty " htmlFor="name">Name:</label>
-                        <input className="bg-Contact outline-0 text-white w-full py-[10px] px-[10px] rounded text-[15px] md:text-[20px] " type="text" name="name" placeholder="Full Name" id="" />
+                        <input className="bg-Contact outline-0 text-white w-full py-[10px] px-[10px] rounded text-[15px] md:text-[20px] " type="text" name="from_name" placeholder="Full Name" id="" />
                     </div>
                     <div>
-                        <label className="text-slate-100 text-[20px] font-fonty " htmlFor="email">email:</label>
-                        <input className="bg-Contact outline-0 text-white w-full py-[10px] px-[10px] rounded text-[15px] md:text-[20px] " type="email" name="email" placeholder="Email" id="" />
+                        <label className="text-slate-100 text-[20px] font-fonty " htmlFor="email">Smail:</label>
+                        <input className="bg-Contact outline-0 text-white w-full py-[10px] px-[10px] rounded text-[15px] md:text-[20px] " type="email" name="from_email" placeholder="Email" id="" />
                     </div>
+        
                     <div className="md:col-span-2 ">
                         <label className="text-slate-100 text-[20px] font-fonty " htmlFor="message">Message:</label>
-                        <textarea className="bg-Contact outline-0 text-white w-full py-[10px] px-[10px] min-h-[100px] rounded text-[15px] md:text-[20px] " name="" id=""placeholder="Write Your Message" ></textarea>
+                        <textarea className="bg-Contact outline-0 text-white w-full py-[10px] px-[10px] min-h-[100px] rounded text-[15px] md:text-[20px] " name="message" id=""placeholder="Write Your Message" ></textarea>
                     </div>
-                    <button className=" self-start bg-slate-200 py-[5px] px-[5px] text-[25px] font-fonty rounded hover:bg-slate-400 " type="submit">Send message</button>
+                    <button  className=" md:float-right transition ease-in delay-150 hover:-translate-y-1 hover:scale-110 w-fit self-start bg-slate-200 py-[5px] px-[5px] text-[25px] font-fonty rounded hover:bg-slate-400 " type="submit">Send message</button>
                 </form>
                 
                     
--- src/Components/Projects/projects.jsx
+++ src/Components/Projects/projects.jsx
@@ -27,56 +27,56 @@ const projectItems = [
         projectName : 'EE-UNIZIK',
         ProjectDescr : 'This A E-Library Website for Electrical Engineering students in UNIZIK where they can download course outlines for each course and also dowload reading resources like handouts, exam past question and textbooks. They can also calculate their CGPA on the website. The website is under production. The link provided below is the demo link. ',
         projectStack: 'Stack/Tools used: React,Tailwindcss,Git,Router',
-        ProjectUrl : 'hh',
-        ProjectGit : 'gg',
+        ProjectGit : 'github.com/DesmondNzubechi/ee-unizik',
+        ProjectUrl : 'ee-unizik.vercel.app',
     },
     {
         ProjectImg: Myhealth,
         projectName : 'My Health',
         ProjectDescr : 'This is a healthcare website where patients can book treatment online from comfort of their home. It  has great user experience',
         projectStack: 'Stack/Tools used: Html, Css, Javascript',
-        ProjectUrl : 'hh',
-        ProjectGit : 'gg',
+        ProjectGit : 'github.com/DesmondNzubechi/Health-Care-Website',
+        ProjectUrl : 'myhealth-by-nzubechukwu.netlify.app',
     },
     {
         ProjectImg: AirtimeSeller,
         projectName : 'Airtime Seller.',
         ProjectDescr : 'I built this website with a modern and responsive user interface that is intuitive and easy to use.',
         projectStack: 'Stack/Tools used: React,Tailwindcss,Git,Vercel&Router',
-        ProjectUrl : 'hh',
-        ProjectGit : 'gg',
+        ProjectGit : 'github.com/DesmondNzubechi/Airtime-seller',
+        ProjectUrl : 'airtimeseller-by-nzubechukwu.vercel.app/',
     },
     {
         ProjectImg: shoppy,
         projectName : 'Shoppy',
         ProjectDescr : 'Shoppy is an ecommerce website where user can buy Wristwatch, Clothes and you. I built this project in practice of my javascript skill. Items can be added and removed from cart.',
         projectStack: 'Stack/Tools used: HTML, CSS, BOOTSTRAP AND JAVASCRIPT.',
-        ProjectUrl : 'hh',
-        ProjectGit : 'gg',
+        ProjectGit : 'github.com/DesmondNzubechi/Ecommerce-Website',
+        ProjectUrl : 'shoppy-by-nzubechukwu.netlify.app',
     },
     {
         ProjectImg: nuesa,
         projectName : 'Nuesa unizik',
         ProjectDescr : 'This a website for faculty of engineering unizik where its students can download reading material and also read article about engineering. I am among the team that built the website. I worked on the frontend,',
         projectStack: 'Stack/Tools used: React, NodeJs, MongoDB, Postman,Tailwindcss,Git,Vercel&Router',
-        ProjectUrl : 'hh',
-        ProjectGit : 'gg',
+        ProjectUrl : 'client-side-coral.vercel.app/',
+        ProjectGit : 'github.com/NUESA-UNIZIK/Client-side',
     },
     {
         ProjectImg: Fintek,
         projectName : 'Fintekk',
         ProjectDescr : 'This is a fintek landing page.I built this website with a modern and responsive user interface that is intuitive and easy to use.',
         projectStack: 'Stack/Tools used: React,Tailwindcss,Git,Vercel&Router',
-        ProjectUrl : 'hh',
-        ProjectGit : 'gg',
+        ProjectUrl : 'fintekk-landing-page.vercel.app/',
+        ProjectGit : 'github.com/DesmondNzubechi/fintekk-landing-page ',
     },
     {
         ProjectImg: readerslodge,
         projectName : 'Readers Lodge',
         ProjectDescr : 'This is a book store that I built. The aim of the site is to give users to read/order and download different kinds of book from their home. The website is still under production but a demo link is provided below.  I built this website with a modern and responsive user interface that is intuitive and easy to use.',
         projectStack: 'Stack/Tools used: React,Tailwindcss,Git,Vercel&Router',
-        ProjectUrl : 'hh',
-        ProjectGit : 'gg',
+        ProjectUrl : 'readerslodge-by-nzubechukwu.vercel.app/',
+        ProjectGit : 'github.com/DesmondNzubechi/Readers-lodge',
     },
    
    
@@ -85,8 +85,8 @@ const projectItems = [
         projectName : 'Home Features.',
         ProjectDescr : 'This is a real a state website where houses are sold.Each house has it location where you can easily locate it.  It is fully responsive and user friendly.',
         projectStack: 'Stack/Tools used: Html, Css, Bootstrap, Javascript',
-        ProjectUrl : 'hh',
-        ProjectGit : 'gg',
+        ProjectUrl : 'homefeatures.netlify.app',
+        ProjectGit : 'github.com/DesmondNzubechi/Property-Management',
     },
   
     {
@@ -94,16 +94,16 @@ const projectItems = [
         projectName : 'CGPA Calculator',
         ProjectDescr : 'This is a CGPA calculator. It is used to calculate students CGPA and it is very accurate.  I built this particular project for the students that finds ut difficult to calculate their CGPA',
         projectStack: 'Stack/Tools used:HTML, CSS AND JAVASCRIPT.',
-        ProjectUrl : 'hh',
-        ProjectGit : 'gg',
+        ProjectUrl  : 'cgpacalcby-nzubechukwu.vercel.app',
+        ProjectGit : 'github.com/DesmondNzubechi/cgpa-calculator',
     },
     {
         ProjectImg: b2rex,
         projectName : 'B2R Exchange',
         ProjectDescr : 'This a website where you can easily trade your gift card without any risk. It has great user experience.',
         projectStack: 'Stack/Tools used: HTML, CSS AND JAVASCRIPT.',
-        ProjectUrl : 'hh',
-        ProjectGit : 'gg',
+        ProjectUrl : 'b2rxchange.netlify.app',
+        ProjectGit : 'github.com/DesmondNzubechi/Gift-Card-Exchange',
     }
   
 ]
@@ -123,15 +123,15 @@ const projectItems = [
         
         <div  className="flex flex-col mb-[150px] md:flex-row gap-[20px] items-end justify-center ">
         <div data-aos='fade-right' data-aos-duration='1000' className="max-w-[600px]  ">
-        <img src={projects.ProjectImg} alt="" className="rounded hover:rounded-[30px] shadow-2xl " />
+        <img src={projects.ProjectImg} alt="" className="rounded transition ease-in delay-300 hover:-translate-y-1 hover:scale-100  shadow-2xl " />
         </div>
 <div data-aos='fade-left' data-aos-duration='1000' className="flex flex-col gap-1 md:gap-4 max-w-[500px] ">
 <h1 className="font-myfont text-[25px] mt-[10px]  ">{projects.projectName}</h1>
 <p className="md:text-[17px] text-slate-300 ">{projects.ProjectDescr}</p>
 <p className="md:text-[17px] text-slate-300 ">{projects.projectStack}</p>
 <p className="flex flex-row gap-2 ">
-    <a href="http://" className="flex hover:bg-slate-50 flex-row text-slate-900 bg-slate-200 rounded px-[5px] py-[5px] gap-1 font-fonty shadow-2xl  text-[17px] items-center">View code <FiGithub className="  font-bold   "/></a>
-    <a href="http://" className="flex border text-slate-200 hover:text-slate-50 px-[10px] rounded gap-2 text-[17px] flex-row items-center">Visit<FaLink className="  font-bold   "/></a>
+    <a href={`https://${projects.ProjectGit}`} className="flex transition ease-in delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-slate-50 flex-row text-slate-900 bg-slate-200 rounded px-[5px] py-[5px] gap-1 font-fonty shadow-2xl  text-[17px] items-center">View code <FiGithub className="  font-bold   "/></a>
+    <a href={`https://${projects.ProjectUrl}`} className="flex transition ease-in delay-150 hover:-translate-y-1 hover:scale-110 border text-slate-200 hover:text-slate-50 px-[10px] rounded gap-2 text-[17px] flex-row items-center">Visit<FaLink className="  font-bold   "/></a>
 </p>
 </div>
     </div>
