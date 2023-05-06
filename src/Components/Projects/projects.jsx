d4084836e2feba2c8038e14b29562df838114199
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-05-06 08:02:14 +0100

    new change

--- src/Components/Projects/projects.jsx
+++ src/Components/Projects/projects.jsx
@@ -25,7 +25,7 @@ const projectItems = [
     {
         ProjectImg: eeunizik,
         projectName : 'EE-NAU',
-        ProjectDescr : 'I\m building this E-Library Website for Electrical Engineering students in my school (UNIZIK) where they can download course outlines for each course and also dowload reading resources like handouts, exam past question and textbooks. They can also calculate their CGPA on the website. The website is under production. Tech is all about giving back to the community so I deceided to give back what I have to the community. The link provided below is the demo link. ',
+        ProjectDescr : 'I\'m building this E-Library Website for Electrical Engineering students in my school (UNIZIK) where they can download course outlines for each course and also dowload reading resources like handouts, exam past question and textbooks. They can also calculate their CGPA on the website. The website is under production. Tech is all about giving back to the community so I deceided to give back  to the community with the skill that I have. The link provided below is the demo link. ',
         projectStack: 'Stack/Tools used: React,Tailwindcss,Git,Router',
         ProjectGit : 'github.com/DesmondNzubechi/ee-nau',
         ProjectUrl : '#',
