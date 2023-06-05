64478a50d7ac140ddb1673366b20bdff20f65dd2
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-06-05 03:12:43 +0100

    new change

--- src/Components/Hero Section/HeroSection.jsx
+++ src/Components/Hero Section/HeroSection.jsx
@@ -17,7 +17,7 @@ export const HeroSection = () => {
         <div  className="flex gap-5 items-center justify-around  flex-col md:flex-row">
        <div data-aos='fade-up' data-aos-duration='1000' className="max-w-[500px] flex flex-col gap-2 ">
         <h1 className="uppercase font-myfont text-[25px] md:text-[40px] text-slate-50  ">Desmond Nzubechukwu</h1>
-        <p className="text-slate-50 text-[16px] md:text-[20px] capitalize ">I am a passionate <strong>frontend web developer</strong> .I create beautiful, intuitive, and user-friendly websites that not only meet the needs of my clients but also exceed the expectations of their users. </p>
+        <p className="text-slate-50 text-[16px] md:text-[20px] capitalize ">I am a passionate <strong>frontend web developer.</strong> <br /> I create beautiful, intuitive, and user-friendly websites that not only meet the needs of my clients but also exceed the expectations of their users. </p>
         <div className="flex flex-row gap-3">
             <Link to='/Contact' className=" ease-in delay-150 hover:-translate-y-1 hover:scale-110 bg-slate-400 text-slate-900 p-1 text-[20px] rounded font-fonty  ">Contact Me</Link> 
             <a href={Resume}  className=" ease-in delay-150 hover:-translate-y-1 hover:scale-110 text-slate-50 p-1 border text-[20px] rounded font-fonty  ">My Resume</a>
